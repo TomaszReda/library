ebcbf114484c1d897c1865347028dc0a286cc93e
TomaszReda <tomekreda16@gmail.com>
2018-10-29 22:01:45 +0100

    AP-5 Main Page view

--- src/main/webapp/src/app/register/register.module.ts
+++ src/main/webapp/src/app/register/register.module.ts
@@ -3,13 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { RegisterRoutingModule } from './register-routing.module';
 import { RegisterComponent } from './register.component';
-import { RegisterrrrComponent } from '../registerrrr/registerrrr.component';
 
 @NgModule({
   imports: [
     CommonModule,
     RegisterRoutingModule
   ],
-  declarations: [RegisterComponent, RegisterrrrComponent]
+  declarations: [RegisterComponent]
 })
 export class RegisterModule { }
