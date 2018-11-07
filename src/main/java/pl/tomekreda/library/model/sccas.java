0746511740350042e5257a233c31d70f5c61df73
tomaszReda <tomasz.reda@xentivo.pl>
2018-11-07 10:53:07 +0100

    AP-2 login Implementation

---
+++ src/main/java/pl/tomekreda/library/model/sccas.java
@@ -0,0 +1,4 @@
+package pl.tomekreda.library.model;
+
+public class sccas {
+}
--- src/main/java/pl/tomekreda/library/security/WebSecurityConfig.java
+++ src/main/java/pl/tomekreda/library/security/WebSecurityConfig.java
@@ -1,25 +1,73 @@
 package pl.tomekreda.library.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.savedrequest.NullRequestCache;
+import org.springframework.session.web.http.HeaderHttpSessionIdResolver;
+import org.springframework.session.web.http.HttpSessionIdResolver;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Configuration
+    class JdbcSessionConfig {
+
+        @Bean
+        public HttpSessionIdResolver httpSessionStrategy() {
+            return HeaderHttpSessionIdResolver.xAuthToken();
+        }
+
+    }
+
+    @Bean
+    public UserDetailsService customUserDetailsService() {
+        return new UserLoginService();
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable()
+        http
                 .authorizeRequests()
                 .antMatchers("/cos").fullyAuthenticated()
+                .antMatchers("/login/**").permitAll()
                 .antMatchers(HttpMethod.GET,"/**").permitAll()
                 .antMatchers(HttpMethod.POST,"/**").permitAll()
                 .antMatchers(HttpMethod.DELETE,"/**").permitAll()
-                .anyRequest().fullyAuthenticated();
+                .anyRequest().fullyAuthenticated()
+                .and()
+                .requestCache()
+                .requestCache(new NullRequestCache())
+                .and()
+                .httpBasic()
+                .and();
+
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+    }
+
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
