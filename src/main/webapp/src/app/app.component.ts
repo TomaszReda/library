6235049b2d4d5a7c09d6427d307fb73e790e7bc4
tomaszReda <tomasz.reda@xentivo.pl>
2018-11-08 08:18:33 +0100

    AP-2 login Implementation

--- src/main/webapp/src/app/app.component.ts
+++ src/main/webapp/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import {Component, ViewChild} from '@angular/core';
 import {NgForm} from "@angular/forms";
 import {AuthService} from "./service/auth.service";
-import {ModalModule} from "angular-custom-modal";
+import {ModalComponent} from "angular-custom-modal";
 
 @Component({
   selector: 'app-root',
@@ -12,15 +12,18 @@ export class AppComponent {
   title = 'webapp';
 
 
+  @ViewChild("loginForm")
+  private loginForm: NgForm;
+
+  @ViewChild("login")
+  public modalLogin: ModalComponent;
 
   constructor(public authService: AuthService) {
-
   }
 
-
   onLogin(loginForm: NgForm) {
-    this.authService.login(loginForm.value.email, loginForm.value.password);
-
+    this.authService.login(loginForm.value.email, loginForm.value.password, this.modalLogin);
+    this.loginForm.resetForm();
   }
 
   logout() {
--- src/main/webapp/src/app/service/auth.service.ts
+++ src/main/webapp/src/app/service/auth.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Credentials} from "../model/user/Credentials";
 import {Router} from "@angular/router";
+import {ModalComponent} from "angular-custom-modal";
 
 @Injectable({
   providedIn: 'root'
@@ -10,29 +10,31 @@ export class AuthService {
 
   public islogin: boolean = false;
 
+  public badLogin: string = null;
+
   constructor(private http: HttpClient, private router: Router) {
   }
 
   url: string = "http://localhost:8080/";
 
-  login(email: string, password: string,) {
+  login(email: string, password: string, modalLogin: ModalComponent) {
     const creditians = {
       email,
       password
     };
     this.http.post(this.url + "login", creditians).subscribe(x => {
+      modalLogin.close();
+      this.badLogin = null;
       this.islogin = true;
-      this.router.navigate(["/home"]);
     }, error1 => {
+      this.badLogin = 'Podaj poprawny login i hasło';
       this.islogin = false;
     })
   }
 
 
   logout() {
-    console.log("aaaa");
     this.router.navigate(["/home"]);
-    console.log("aaaa");
 
     this.http.get(this.url + "logout").subscribe(x => {
       this.islogin = false;
