f0094fe8cd87a5aa215f703b68e0ddce0d049a3d
tomaszReda <tomasz.reda@xentivo.pl>
2018-11-21 12:36:16 +0100

    AP-18 add library implementation

--- src/main/webapp/src/app/add-library/add-library.component.ts
+++ src/main/webapp/src/app/add-library/add-library.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var ol: any;
 @Component({
   selector: 'app-add-library',
   templateUrl: './add-library.component.html',
@@ -7,9 +8,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddLibraryComponent implements OnInit {
 
-  constructor() { }
+  public latitude: number = 21.0158;
+  public longitude: number = 52.2051;
+
+  map: any;
 
   ngOnInit() {
+    var mousePositionControl = new ol.control.MousePosition({
+      coordinateFormat: ol.coordinate.createStringXY(4),
+      projection: 'EPSG:4326',
+      // comment the following two lines to have the mouse position
+      // be placed within the map.
+      className: 'custom-mouse-position',
+      target: document.getElementById('mouse-position'),
+      undefinedHTML: '&nbsp;'
+    });
+
+
+    this.map = new ol.Map({
+      target: 'map',
+      controls: ol.control.defaults({
+        attributionOptions: {
+          collapsible: false
+        }
+      }).extend([mousePositionControl]),
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        })
+      ],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([this.latitude, this.longitude]),
+        zoom: 8
+      })
+    });
+
+    this.map.on('click', function (args) {
+      var lonlat = ol.proj.transform(args.coordinate, 'EPSG:3857', 'EPSG:4326');
+
+      var lon = lonlat[0];
+      var lat = lonlat[1];
+
+
+      // alert(`lat: ${lat} long: ${lon}`);
+    });
   }
 
+  setCenter() {
+    var view = this.map.getView();
+    view.setCenter(ol.proj.fromLonLat([this.longitude, this.latitude]));
+    view.setZoom(8);
+  }
 }
--- src/main/webapp/src/app/add-library/add-library.module.ts
+++ src/main/webapp/src/app/add-library/add-library.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { AddLibraryRoutingModule } from './add-library-routing.module';
 import { AddLibraryComponent } from './add-library.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     AddLibraryRoutingModule
   ],
   declarations: [AddLibraryComponent]
