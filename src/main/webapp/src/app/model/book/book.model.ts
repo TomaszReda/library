1a35fe34935dd4054f508742d4935aab6c2f4085
TomaszReda <tomekreda16@gmail.com>
2018-12-02 23:28:23 +0100

    AP-20 Add form to add Book

---
+++ src/main/webapp/src/app/model/book/book.model.ts
@@ -0,0 +1,10 @@
+import {FormControl} from "@angular/forms";
+
+export class Book {
+  title?: string;
+  ISBN?: string;
+  author?: string;
+  publisher?: string;
+  date?: Date;
+  quant?: number;
+}
--- src/main/webapp/src/app/my-library/add-book-to-library/add-book-to-library.component.ts
+++ src/main/webapp/src/app/my-library/add-book-to-library/add-book-to-library.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, NgForm, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-add-book-to-library',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddBookToLibraryComponent implements OnInit {
 
-  constructor() { }
+  public formAddBook: FormGroup;
+
+  public submit = false;
+
+  constructor() {
+  }
 
   ngOnInit() {
+    this.initForms();
+  }
+
+  initForms() {
+    this.formAddBook = new FormGroup({
+      author: new FormControl(null, [Validators.required]),
+      title: new FormControl(null, [Validators.required]),
+      publisher: new FormControl(null, [Validators.required]),
+      date: new FormControl(null, [Validators.required]),
+      ISBN: new FormControl(null, [Validators.required]),
+      quant: new FormControl(null, [Validators.required, Validators.min(1)]),
+    })
+  }
+
+
+  addBooks() {
+    this.submit = true;
+  }
+
+  reset() {
+    this.initForms();
+    this.submit = false;
   }
 
 }
