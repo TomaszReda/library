5884f9543e76e13e827b83985a503454aa8e476b
TomaszReda <tomekreda16@gmail.com>
2018-12-13 00:03:20 +0100

    AP-40  deploy application on server

--- src/main/webapp/src/environments/environment.prod.ts
+++ src/main/webapp/src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
+  // Url to local work
   url: 'http://localhost:8080/api/',
-  // url: 'http:///80.211.75.241:8080:8080/api/'
+  //Url to server work
+  //  url: 'http:///80.211.75.241:8080:8080/api/'
 };
