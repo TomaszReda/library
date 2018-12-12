7bc60b6ddeed03458009ec89e8c1ccbb9f96b549
tomaszReda <tomasz.reda@xentivo.pl>
2018-12-12 14:29:25 +0100

    AP-43 Add delete reservation

--- src/main/java/pl/tomekreda/library/model/user/User.java
+++ src/main/java/pl/tomekreda/library/model/user/User.java
@@ -2,31 +2,20 @@ package pl.tomekreda.library.model.user;
 
 import lombok.Data;
 
-import javax.persistence.*;
-import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.util.UUID;
 
 @Entity
 @Data
-public class User implements Serializable {
+public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private UUID ID;
+    @GeneratedValue
+    private UUID IDDD;
 
-    private String firstname;
+    private String firstnameasdasdadada;
 
-    private String lastname;
 
-    private String email;
-
-    private String phoneNumber;
-
-    private String password;
-
-    @OneToOne(cascade = CascadeType.ALL)
-    private UserMenager userMenager;
-
-    @OneToOne(cascade = CascadeType.ALL)
-    private UserCasual userCasual;
 }
