6a2d23d791401c4b688f0009b94e60932ab1fc3c
TomaszReda <tomekreda16@gmail.com>
2018-12-27 23:31:29 +0100

    AP-55 add view to admin panel

--- src/main/webapp/src/app/forAdmin/admin-panel/admin-panel-routing.module.ts
+++ src/main/webapp/src/app/forAdmin/admin-panel/admin-panel-routing.module.ts
@@ -1,6 +1,9 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {AdminPanelComponent} from "./admin-panel.component";
+import {BookListComponent} from "./book-list/book-list.component";
+import {LibraryListComponent} from "./library-list/library-list.component";
+import {UserListComponent} from "./user-list/user-list.component";
 
 const routes: Routes = [
   {
@@ -8,9 +11,16 @@ const routes: Routes = [
     component: AdminPanelComponent,
     children: [
       {
-        path: '',
-        component: AdminPanelComponent,
-
+        path: "book/list",
+        component: BookListComponent,
+      },
+      {
+        path: "library/list",
+        component: LibraryListComponent,
+      },
+      {
+        path: "user/list",
+        component: UserListComponent
       }
     ]
   }
--- src/main/webapp/src/app/forAdmin/admin-panel/admin-panel.module.ts
+++ src/main/webapp/src/app/forAdmin/admin-panel/admin-panel.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { AdminPanelRoutingModule } from './admin-panel-routing.module';
 import { AdminPanelComponent } from './admin-panel.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { LibraryListComponent } from './library-list/library-list.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   imports: [
     CommonModule,
     AdminPanelRoutingModule
   ],
-  declarations: [AdminPanelComponent]
+  declarations: [AdminPanelComponent, BookListComponent, LibraryListComponent, UserListComponent]
 })
 export class AdminPanelModule { }
