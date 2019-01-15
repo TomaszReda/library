64ffb011a0ee03c523d36aac090a89226cdc5e03
TomaszReda <tomekreda16@gmail.com>
2019-01-15 21:17:52 +0100

    AP-80 notification front-end implementation

--- src/main/java/pl/tomekreda/library/controller/MessageToCasualUserController.java
+++ src/main/java/pl/tomekreda/library/controller/MessageToCasualUserController.java
@@ -4,26 +4,22 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import pl.tomekreda.library.service.MessageService;
+import pl.tomekreda.library.service.MessageToCasualUserService;
 
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
-public class MessageController {
+public class MessageToCasualUserController {
 
-    private final MessageService messageService;
+    private final MessageToCasualUserService messageService;
 
     @GetMapping("/notification/for/casual/user")
     public ResponseEntity getAllUnreadNotificationForCasualUser(@RequestParam int page, @RequestParam int size) {
         return messageService.getAllUnreadNotificationForCasualUser(page,size);
     }
 
-    @GetMapping("/notification/for/library/owner")
-    public ResponseEntity getAllUnreadNotificationForLibraryOwner(@RequestParam int page, @RequestParam int size) {
-        return messageService.getAllUnreadNotificationForLibraryOwner(page,size);
-    }
 
     @PostMapping("/read/notification")
     public ResponseEntity readNotification(@RequestBody UUID messageId) {
