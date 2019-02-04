89767878326687b319d941c51da7cb130b79f87b
tomek <tomekreda16@gmail.com>
2019-02-04 21:29:21 +0100

    AP-86 web socket end implementation

--- src/main/webapp/src/environments/environment.prod.ts
+++ src/main/webapp/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  // url: 'http://localhost:8080/api',
+  url: 'http://localhost:8080/api',
   webSocketUrl: 'http://localhost:8080/',
-  url: 'http://80.211.75.241:8080/api',
+  // url: 'http://80.211.75.241:8080/api',
   timeToNotification: 30000
 
 };
